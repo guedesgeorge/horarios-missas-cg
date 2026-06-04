65bb3a5aa48c7b23d227a27aee7dfe372b684ea3
george <george.emannuelcarvalho@gmail.com>
2026-06-04 16:45:18 -0400

    atualizacao

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const PORT = process.env.PORT || 3000;
+ 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Missas CG rodando na porta 3000');
+ 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Missas CG rodando na porta ${PORT}`);
 });
+ 
